c101f241dd06af50327ecae9f14fd7669e4821f8
ryanraidt <ryanraidt1026@gmail.com>
2024-11-04 11:44:58 -0600

    final commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//Create let message = You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+//Create 3 const variables using unique arithmetic operator(+*/) to equal vault combinations
+//Use num1, num2, and num3 as your 3 const variables
+//Make num1 = 10
+//Make num2 = 40
+//Make num3 = 39
+//Add Alert for message, num1, num2, num3
+let message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+// Added the 3 const variables for combination below
+const num1 = 5 * 2;
+const num2 = 39 + 1;
+const num3 = 78 / 2;
+//Added alert for the message and const variables below
+alert(`${message} ${num1}-${num2}-${num3}`);
